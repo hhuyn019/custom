1d9f8a23a5f5e1237e363e21e829fd324217289e
hhuyn019 <38028924+hhuyn019@users.noreply.github.com>
2019-11-26 03:36:52 -0800

    Update main.c

--- main.c
+++ main.c
@@ -23,6 +23,57 @@
 
 /*-------------------------------------------------------------------------*/
 
+typedef enum Direction{NONE, LEFT, RIGHT, UP, DOWN} Direction;
+	
+#define JOYSTICK_LIMIT_LEFT 100
+#define JOYSTICK_LIMIT_RIGHT 1000
+#define JOYSTICK_LIMIT_UP 100
+#define JOYSTICK_LIMIT_DOWN 1000
+
+
+typedef struct Joystick_Frame {
+	
+	unsigned short raw_x;
+	unsigned short raw_y;
+	unsigned char click;
+	
+	//These will be filled by 2 calls to Joystick_Process_Raw();
+	Direction X_direction;
+	Direction Y_direction;
+} Joystick_Frame;
+
+
+void Joystick_Process_Raw(Joystick_Frame* frame);
+void Joystick_Read(Joystick_Frame* frame);
+void Joystick_Tick();
+
+void Joystick_Process_Raw(Joystick_Frame* frame){
+	
+	//Set X
+	if(frame->raw_x < JOYSTICK_LIMIT_LEFT){frame->X_direction = LEFT;}
+	else if(frame->raw_x > JOYSTICK_LIMIT_RIGHT){frame->X_direction = RIGHT;}
+	else{frame->X_direction = NONE;}
+	//Set Y
+	if(frame->raw_y < JOYSTICK_LIMIT_UP){frame->Y_direction = UP;}
+	else if(frame->raw_y > JOYSTICK_LIMIT_DOWN){frame->Y_direction = DOWN;}
+	else{frame->Y_direction = NONE;}
+	//Decide what to do on ambiguous inputs:
+	//I have decided to ignore the vertical input as it isn't 
+	//time critical to move the block downward
+	if(frame->X_direction != NONE && frame->Y_direction != NONE){
+		frame->Y_direction = NONE;
+	} 
+}
+
+void Joystick_Read(Joystick_Frame* frame){
+	
+	//We will read the Vx and Vy from the 2-potentiometer Joystick;
+	
+	frame->raw_x = ADC_read(0);		//read 10 bit ADC value on ADC0	
+	frame->raw_y = ADC_read(1);		//read ADC1 as well
+	frame->click = (~PINA & 0x08) ? 1 : 0; //read the click
+}
+
 void LCD_init();
 void LCD_ClearScreen(void);
 void LCD_WriteCommand (unsigned char Command);
